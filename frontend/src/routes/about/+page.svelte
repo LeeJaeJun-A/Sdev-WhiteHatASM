1464d26a39e717bee3c166a9a8cb73c7b648a480
LeeJaeJun-A <sjaqjnjs@naver.com>
2024-08-09 23:38:08 +0900

    feat: Update about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main class="bg-white w-screen h-screen">
-  <NavBar/>
+  <NavBar />
   <div class="w-full flex items-center justify-center" style="height: 92%;">
-    This is about
+    <h1>This is about</h1>
   </div>
 </main>
